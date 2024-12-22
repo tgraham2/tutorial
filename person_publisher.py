e2ee6eacf00237d6c1fd168293420dbd832f505b
Tom Graham <tg7361@yahoo.com>
2024-12-22 10:22:02 -0500

    p_pub_indents

--- person_publisher.py
+++ person_publisher.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- #This routine will pulish /person_info topic and customize message type
-beginner_hiwonder::Person.
+# -*- coding: utf-8 -*- 
+#This routine will pulish /person_info topic and customize message type beginner_hiwonder::Person
 
 import rospy
 from beginner_hiwonder.msg import Person
@@ -9,7 +9,7 @@ def velocity_publisher():
   # Initialize ROS node
   rospy.init_node('person_publisher', anonymous=True)
   # Create a publisher and publish a topic named /person_info. The message type is
-beginner_hiwonder::Person and the queue length is 10.
+  # beginner_hiwonder::Person and the queue length is 10.
 
 person_info_pub = rospy.Publisher('/person_info', Person, queue_size=10)
   #set the loop rate
@@ -22,12 +22,12 @@ person_info_pub = rospy.Publisher('/person_info', Person, queue_size=10)
     person_msg.age = 18;
     person_msg.sex = Person.male;
 
-  # publish message
-  person_info_pub.publish(person_msg)
-  rospy.loginfo("Publsh person message[%s, %d, %d]", person_msg.name, person_msg.age, person_msg.sex)
+    # publish message
+    person_info_pub.publish(person_msg)
+    rospy.loginfo("Publsh person message[%s, %d, %d]", person_msg.name, person_msg.age, person_msg.sex)
 
-  # Delay on basis of the loop rate
-  rate.sleep()
+    # Delay on basis of the loop rate
+    rate.sleep()
 
 if __name__ == '__main__':
   try:
